54bac051f6a591c5f175e7bfa0c0eb302af425e1
Kiet Nguyen <kietnguyen@nexondv.com>
2023-09-11 17:32:59 +0700

    update home flash sale section with infinite product slide

--- src/app/features/home/components/home-flash-sale-products/home-flash-sale-products.component.ts
+++ src/app/features/home/components/home-flash-sale-products/home-flash-sale-products.component.ts
@@ -1,12 +1,92 @@
 import { products } from "@/data/products";
 import { TProducts } from "@/types";
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: "app-home-flash-sale-products",
   templateUrl: "./home-flash-sale-products.component.html",
   styleUrls: ["./home-flash-sale-products.component.css"],
 })
-export class HomeFlashSaleProductsComponent {
-  products: TProducts = products;
+export class HomeFlashSaleProductsComponent implements OnInit, OnDestroy {
+  products: TProducts = products.slice(0, 10);
+  requestId: number | undefined;
+  elementsX: Array<number> = [];
+  previousTimeStamp: number = 0;
+  slideSpeed = 0.1;
+  PRODUCT_CARD_WIDTH: number = 240;
+  PRODUCT_CARDS_GAP: number = 16;
+
+  slowDownSlideSpeed() {
+    this.slideSpeed = 0.025;
+  }
+
+  speedUpSlideSpeed() {
+    this.slideSpeed = 0.1;
+  }
+
+  ngOnInit(): void {
+    const wrapperElement = document.getElementById(
+      "flash-sale-products-card-wrapper"
+    ) as HTMLElement;
+
+    const listCardElements = wrapperElement.getElementsByClassName(
+      "product-card"
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    const getProductsSlide = (timeStamp: number) => {
+      if (this.previousTimeStamp === 0) {
+        // this branch run one time in the very first time
+        // when the timeStamp === 0
+
+        for (let element of listCardElements) {
+          // init each element x position
+          this.elementsX.push(0);
+
+          // add event listener when hover the card
+          element.addEventListener("mouseenter", () =>
+            this.slowDownSlideSpeed()
+          );
+
+          element.addEventListener("mouseleave", () =>
+            this.speedUpSlideSpeed()
+          );
+        }
+
+        this.previousTimeStamp = timeStamp;
+      }
+      const elapsedTime = timeStamp - this.previousTimeStamp;
+      this.previousTimeStamp = timeStamp;
+
+      this.elementsX = this.elementsX.map((x, i) => {
+        if (listCardElements[i].getBoundingClientRect().right < 0) {
+          // reset position to the last position of the queue
+          // if product card pass the left edge of the screen
+          return (
+            x +
+            this.PRODUCT_CARD_WIDTH * listCardElements.length +
+            this.PRODUCT_CARDS_GAP * (listCardElements.length - 1) +
+            this.PRODUCT_CARDS_GAP
+          );
+        } else {
+          return x - this.slideSpeed * elapsedTime;
+        }
+      });
+
+      for (let i = 0; i < this.elementsX.length; i++) {
+        listCardElements[
+          i
+        ].style.transform = `translateX(${this.elementsX[i]}px)`;
+      }
+
+      this.requestId = requestAnimationFrame(getProductsSlide);
+    };
+
+    this.requestId = requestAnimationFrame(getProductsSlide);
+  }
+
+  ngOnDestroy(): void {
+    if (this.requestId) {
+      cancelAnimationFrame(this.requestId);
+    }
+  }
 }
